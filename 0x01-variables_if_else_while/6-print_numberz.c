dc7f2533744f314abda9de3ed29364dbc403fcf2
okoromi esosa <okoromiesosa@gmail.com>
2022-07-02 13:06:30 -0700

    c programming continues

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio>
+
+/**
+ * main - files prints single digits numbers
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int p;
+
+	for (p = '0'; p <= '9'; p++)
+	{
+		putchar(p);
+	}
+	putchar('\n');
+	return (0);
+}
