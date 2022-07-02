a3da672ce83c6e8e50524a07eb053c61a762a1a0
okoromi esosa <okoromiesosa@gmail.com>
2022-07-02 13:22:35 -0700

    c programming continues

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - file prints alphabets in reverse
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char h;
+
+	for (h = 'z'; h <= 'a'; h--)
+	{
+		putchar(h);
+	}
+	putchar('\n');
+	return (0);
+}
