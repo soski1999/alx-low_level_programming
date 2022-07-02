e5726ce3c592b47de7d4402fd6ace702bc9a8cbb
okoromi esosa <okoromiesosa@gmail.com>
2022-07-02 12:26:53 -0700

    c programming continues

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - file prints lower and upper case alphabets
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+	for (c = 'A'; c <= 'Z'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
