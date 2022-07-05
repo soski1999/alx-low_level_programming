abcc7e4779f5f75c7af4a47744444a4e7829664d
okoromi esosa <okoromiesosa@gmail.com>
2022-07-05 08:31:45 -0700

    c programming is awesome

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <main.h>
+#include "main.h"
 
 /**
  * main - file prints out _putchar
@@ -8,7 +8,7 @@
 int main(void)
 {
 	_putchar('_');
-	_putchar('p');
+	_putchar(i'p');
 	_putchar('u');
 	_putchar('t');
 	_putchar('c');
