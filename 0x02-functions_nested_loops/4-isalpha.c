e20b096885c5c6ce254d085533239a92f295e770
okoromi esosa <okoromiesosa@gmail.com>
2022-07-05 07:58:46 -0700

    c programming is awesome

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include <main.h>
+
+/**
+ * main - determines if the character is an alphabet
+ * _isalpha determines the character is an alphabet
+ * @c: the character to be checked
+ *
+ * Return: 1 if c is a letter lowercase or uppercase or 0 if anything else
+ */
+int _isalpha(int c);
+{
+if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+{
+	return (1);
+}
+return (0);
+}
