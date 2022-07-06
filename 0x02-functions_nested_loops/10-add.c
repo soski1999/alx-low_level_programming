a8989b7055e5f68e202e2f21eedb3520f71fea01
okoromi esosa <okoromiesosa@gmail.com>
2022-07-06 02:57:07 -0700

    c programming is awesome

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - adds up two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: result of addition
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
