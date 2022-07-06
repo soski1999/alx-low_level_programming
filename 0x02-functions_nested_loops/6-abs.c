4644e8a883011affc5608c59a2ec75539eb925c9
okoromi esosa <okoromiesosa@gmail.com>
2022-07-06 04:17:46 -0700

    c programming is awesome

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,9 +2,18 @@
 
 /**
  * _abs - prints the absolute value of an integer
+ * @n: the integer
  *
- * Return: void
+ * Return: the absolute value of the integer
  */
-int _abs(int)
+int _abs(int n)
 {
-
+	if (n >= 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (-n);
+	}
+}
