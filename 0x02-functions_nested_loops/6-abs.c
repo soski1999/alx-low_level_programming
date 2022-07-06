9b131408c0342b623b0e6c5962a22f55961a63f5
okoromi esosa <okoromiesosa@gmail.com>
2022-07-06 02:41:32 -0700

    c programming is awesome

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ * _abs - prints the absolute value of an integer
+ *
+ * Return: void
+ */
+int _abs(int)
+{
+
---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints last digit of a number
+ * @r: last digit of a number
+ *
+ * Return: value of last digit of the number
+ */
+int print_last_digit(int r)
+{
+	int last;
+
+	last = n % 10;
+	if (last < 0)
+	{
+		last = last * -1;
+	}
+	_putchar(last + '0');
+	return (last)
+}
