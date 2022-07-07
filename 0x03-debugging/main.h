fc618131e0333d14f2ac7e2450ca6ea3ee0c8393
okoromi esosa <okoromiesosa@gmail.com>
2022-07-07 01:50:03 -0700

    c programming is awesome

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -10,52 +10,28 @@
 
 void positive_or_negative(int i);
 
-
-
 #endif /* MAIN_H */
 
-#include <stdio.h>
 
+#include <stdio.h>
 #include "main.h"
 
-
-
 /**
+ * main - prints the largest of 3 integers
  *
- * * main - prints the largest of 3 integers
+ * Return: 0
  *
- * * Return: 0
- *
- * */
-
-
+ */
 
 int main(void)
-
 {
+	int a,  b, c;
+	int largest;
 
-	        int a, b, c;
-
-		        int largest;
-
-
-
-			        a = 972;
-
-				        b = -98;
-
-					        c = 0;
-
-
-
-						        largest = largest_number(a, b, c);
-
-
-
-							        printf("%d is the largest number\n", largest);
-
-
-
-								        return (0);
-
+	a = 972;
+	b = -98;
+	c = 0;
+	largest = largest_number(a, b, c);
+	printf("%d is the largest number\n", largest);
+	return (0);
 }
