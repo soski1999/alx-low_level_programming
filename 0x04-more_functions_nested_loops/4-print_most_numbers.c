d9aee45ea26760cdd00730311ce18b71201721fa
okoromi esosa <okoromiesosa@gmail.com>
2022-07-07 07:43:00 -0700

    c programming is awesome

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints numbers from 0 to 9 except 2 and 4
+ *
+ * Return: numbers from 0 to 9
+ */
+void print_most_numbers(void)
+{
+	int y;
+
+	for (y = 0; y <= 9; y++)
+	{
+		if ( y == 2 || y == 4)
+		{
+			continue
+		}
+		else
+		{
+			_putchar(y);
+		}
+	}
+	_putchar('\n');
+}
