9bdce2995752ebe97e21e06b60fba1ffe648ae0a
okoromi esosa <okoromiesosa@gmail.com>
2022-07-07 01:29:05 -0700

    c programming is awesome

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -5,28 +5,20 @@
  *
  * Return: 0
  */
-
 int main(void)
-
 {
-
 	int i;
 
-
-
-	printf("Infinite loop incoming :(\n");
-
-
-
+	printf("infinite loop incoming :(\n");
 	i = 0;
-	/* while (i < 10)*/
-	/*{*/
-	/*infinite loop - No increase of variable*/
-	/*putchar(i);*/
-	/*}*/
 
-	printf("Infinite loop avoided! \\o/\n");
-
+	/*
+	 * while (i < 10)
+	 * {
+	 * infinite loop - no increase of variable
+	 * putchar(i);
+	 * }
+	 */
+	printf("infinite loop avoided! \\o/\n");
 	return (0);
-
 }
