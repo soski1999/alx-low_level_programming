3c0d3b4ae714fa7c178eabe3e598ef98054c35b8
okoromi esosa <okoromiesosa@gmail.com>
2022-07-07 07:16:59 -0700

    c programming is awesome

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_numbers - print numbers between 0 and 9
+ *
+ * Return: void
+ */
+void print_numbers(void)
+{
+	for (c = 0; c <= 9; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
