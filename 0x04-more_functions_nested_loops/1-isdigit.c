5024e488699dddb01c094703023dca92b6e3f066
okoromi esosa <okoromiesosa@gmail.com>
2022-07-07 07:28:47 -0700

    c programming is awesome

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -4,11 +4,9 @@
  *  _isdigit - checks for number 0 to 9
  *  @c: number to be checked
  *
- *  Return 1 if number is between 0 to 9 and 0 for anything else
+ *  Return: 1 if number is between 0 to 9 and 0 for anything else
  */
-
 int _isdigit(int c)
-
 {
 	if (c >= 48 && c <= 57)
 	{
