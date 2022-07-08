a6d0aec594319b102afcee8736c354b65995e44c
okoromi esosa <okoromiesosa@gmail.com>
2022-07-08 01:11:34 -0700

    c programming is awesome

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints numbers 0 t0 14 10times
+ *
+ * Return: numbers 0 to 14
+ */
+void more_numbers(void)
+{
+	int x;
+	int y;
+
+	for (x = 0; x <= 10; x++)
+	{
+		for (y = 0; y <= 14; y++)
+		{
+			if (y > 9)
+			{
+				_putchar(y / 10) + '0');
+			}
+			_putchar(y % 10) + 0');
+		}
+		_putchar('\n');
+	}
+}
