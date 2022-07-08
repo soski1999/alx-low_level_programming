381e1c5bf405d215b1e2aa9754cfe6275ef46ce6
okoromi esosa <okoromiesosa@gmail.com>
2022-07-08 03:25:46 -0700

    c programming is awesome

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,26 @@
+#include <math.h>
+#include <stdio.h>
+
+/**
+ * main - prints largest prime factor of 612852475143
+ *
+ * Return: void
+ */
+int main(void)
+{
+	long p;
+	maxf;
+
+	long number = 612852475143;
+	double square = sqrt(number);
+
+	for  (p = 1; p <= square; p++)
+	{
+		if (number % p == 0)
+		{
+			maxf = number / p;
+		}
+	}
+	printf("%ld\n", maxf);
+	return (0);
+}
