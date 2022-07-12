b8f0f29f97eed9c40e48003fa8d1587f72350f08
okoromi esosa <okoromiesosa@gmail.com>
2022-07-12 07:24:56 -0700

    c programming is awesome

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - takes pointer to an int with value of 98
+ * @*n: pionter declaration
+ *
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
