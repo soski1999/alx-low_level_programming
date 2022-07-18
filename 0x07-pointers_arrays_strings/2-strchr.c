d078c0e1a9e11227ada69f68c6197bc74bedebb4
okoromi esosa <okoromiesosa@gmail.com>
2022-07-18 12:15:40 -0700

    programming

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strchr - locates a character in a string
+ * @s: source string
+ * @c: character to find
+ *
+ * Return: a pointer or null
+ */
+char *_strchr(char *s, char c)
+{
+	int a = 0, b;
+	while (s[a])
+	{
+		a++
+	}
+	for (b = 0; b <= a; b++)
+	{
+		if (c == s[b])
+		{
+			s += b;
+		return (s);
+		}
+	}
+	return ('\0');
+}
