f7f43eddfd05f21dab01f04f389bcaab9a5d8cd1
okoromi esosa <okoromiesosa@gmail.com>
2022-07-18 12:54:16 -0700

    programming

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -4,3 +4,4 @@ char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 void print_diagsums(int *a, int size);
 void print_chessboard(char (*a)[8]);
+unsigned int _strspn(char *s, char *accept);
