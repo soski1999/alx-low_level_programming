148ead7f69de4fa53daa414bf71e7956fb702240
okoromi esosa <okoromiesosa@gmail.com>
2022-08-01 04:31:57 -0700

    structures

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,22 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * init_dog - initializes avariable of type struct dog
+ * @d: a dog structure
+ * @name: name of dog
+ * @age: dogs age
+ * @owner: name of dog owner
+ *
+ * Return: void
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d == NULL)
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
