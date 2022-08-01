c038ca8f4a49224ef4869af5b573e14c9ab40743
okoromi esosa <okoromiesosa@gmail.com>
2022-08-01 05:08:15 -0700

    structures

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * new_dog - creates a structure for a new dog
+ * @name: name of new dog
+ * @age: age of new dog
+ * @owner: owner of dog
+ *
+ * Return: void
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+
