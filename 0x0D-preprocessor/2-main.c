33a311bebf223fd412d13644217bc2e36edd8597
okoromi esosa <okoromiesosa@gmail.com>
2022-08-01 03:04:26 -0700

    preprocessor

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints file name of the program
+ *
+ * Return: zero
+ */
+int main(void)
+{
+	printf("%s", __FILE__);
+	printf("%s\n", __FILE__);
+	return (0);
+}
