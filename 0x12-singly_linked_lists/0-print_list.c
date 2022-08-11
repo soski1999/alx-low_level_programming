0a60757930270f34f34f470b0505f88d5386f563
okoromi esosa <okoromiesosa@gmail.com>
2022-08-11 12:48:29 -0700

    linkslinks

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * print_list - prints all the elements in a list
+ * @h: a linked list
+ *
+ * Return: number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	size_t count = 0;
+
+	while (h != 0)
+	{
+		if (h->str == NULL)
+			print("[0] (nil)\n");
+		else
+			print("[%d] (%s)\n", h->len, h->str);
+			h = h->next;
+			count ++;
+	}
+	return (count);
+}
