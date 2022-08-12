a547978ce57bf3755cefb252883121e159162fef
okoromi esosa <okoromiesosa@gmail.com>
2022-08-12 02:54:52 -0700

    my commit

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/*
+ * free_list - free list
+ * @head: list
+ *
+ * Return: void
+ */
+void free_list(list_t *head)
+{
+	list_t *n;
+
+	while (head)
+	{
+		n = head;
+		head = head->next;
+		free(n->str);
+		free(n);
+	}
+	return (head);
+}
