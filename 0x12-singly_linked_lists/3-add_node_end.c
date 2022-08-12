b1344510af1761827bcbd687eacc15931cb48680
okoromi esosa <okoromiesosa@gmail.com>
2022-08-12 03:15:51 -0700

    my commit

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+
+/**
+ * add_node_end - add a ndoe at end of list
+ * @head: head of the link list
+ * @str: string
+ *
+ * Return: address of new element
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	size_t n;
+	list_t *new, *temp;
+
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+	new->str = strdup(str);
+	for (n = 0; str[n]; n++)
+		;
+	new->len = n;
+	new->next = NULL;
+	temp = *head;
+	if (temp == NULL)
+	{
+		*head = new;
+	}
+	else
+	{
+		while (temp->next != NULL)
+			temp = temp->next;
+		temp->next = new;
+	}
+	return (*head);
+}
--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -11,7 +11,7 @@ void free_list(list_t *head)
 {
 	list_t *n;
 
-	while (head)
+	while (head != NULL)
 	{
 		n = head;
 		head = head->next;
