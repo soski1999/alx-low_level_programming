d4345e3ef13f3b77a20ff77d8309228f9e246111
okoromi esosa <okoromiesosa@gmail.com>
2022-08-15 14:16:27 -0700

    my commit

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -12,13 +12,15 @@ size_t print_list(const list_t *h)
 {
 	size_t count = 0;
 
-	while (h != 0)
+	while (h != NULL)
 	{
 		if (h->str == NULL)
-			printf("[0] (nil)\n");
+		{
+			printf("[%d] %s\n", 0, "(nil)");
+		}
 		else
 		{
-			printf("[%d] (%s)\n", h->len, h->str);
+			printf("[%d] %s\n", h->len, h->str);
 		}
 			h = h->next;
 			count++;
