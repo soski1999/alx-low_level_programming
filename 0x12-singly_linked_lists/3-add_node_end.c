fcbbc5714b776f0cdaabe149cc6740b69569f5ca
okoromi esosa <okoromiesosa@gmail.com>
2022-08-16 01:56:31 -0700

    my commit

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -28,7 +28,7 @@ list_t *add_node_end(list_t **head, const char *str)
 	else
 	{
 		while (temp->next != NULL)
-			temp = temp->next;
+		temp = temp->next;
 		temp->next = new;
 	}
 	return (*head);
--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,5 +1,4 @@
 #include "lists.h"
-#include <stdlib.h>
 
 /**
  * free_list - free list
