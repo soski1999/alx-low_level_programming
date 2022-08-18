c10e43821069c363169e59667d6590cfda1be089
okoromi esosa <okoromiesosa@gmail.com>
2022-08-18 03:18:54 -0700

    my commit

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * free_listint - free lists
+ * @head: first node or a list
+ *
+ * Return: void
+ */
+void free_listint(listint_t *head)
+{
+	listint_t h;
+
+	if (head == NULL)
+	{
+		free(head);
+	}
+	while (head != NULL)
+	{
+		h = head;
+		head = head->next;
+		free(h);
+	}
+}
