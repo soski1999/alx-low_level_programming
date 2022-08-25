3870e9b297f6b7ab27e42ee66d35cbd355b50600
okoromi esosa <okoromiesosa@gmail.com>
2022-08-25 02:05:18 -0700

    my commit

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,27 +9,22 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t fd;
+	int fd;
 	ssize_t readed;
 	ssize_t pwrite;
 	char *buf;
 
-	buf = malloc(sizeof(char) * letters);
-
-	if (buf == NULL)
-		return (0);
 	if (filename == NULL)
 		return (0);
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
+	buf = malloc(sizeof(char) * (letters));
+	if (!buf)
+		return (0);
 	readed = read(fd, buf, letters);
-	if (readed == -1)
-		return (0);
 	pwrite = write(STDOUT_FILENO, buf, readed);
-	if (pwrite == -1)
-		return (0);
-	free(buf);
 	close(fd);
+	free(buf);
 	return (pwrite);
 }
