bc02e2b45feb1cc654825c4187268cd12170a3ef
okoromi esosa <okoromiesosa@gmail.com>
2022-08-25 01:40:33 -0700

    my commit

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,3 +9,22 @@
  */
 int create_file(const char *filename, char *text_content)
 {
+	int nletters;
+	int fd;
+	int wrletters;
+
+	if (filename == NULL)
+		return (-1);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+	if (text_content == NULL)
+	text_content = "";
+	for (nletters = 0; text_content[nletters]; nletters++)
+		;
+	wrletters = write(fd, text_content, nletters);
+	if (wrletters == -1)
+		return (-1);
+	close(fd);
+	return (1);
+}
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -8,4 +8,5 @@
 #include <sys/uio.h>
 #include <unistd.h>
 size_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
 #endif
